e7cb27de61134aa58f9879d2e0043b8724d4c0bd
Khan-solo <mursilkhan30@gmail.com>
2022-05-25 01:03:28 +0530

    Removed unused imports

--- HtmlGenerator.py
+++ HtmlGenerator.py
@@ -1,16 +1,7 @@
 from selenium import webdriver
 from selenium.webdriver.chrome.service import Service
 from webdriver_manager.chrome import ChromeDriverManager
-from selenium.webdriver.chrome.options import Options
-from selenium.webdriver.common.action_chains import ActionChains
-from selenium.webdriver.common.keys import Keys
-from selenium.webdriver.common.by import By
-from time import sleep
 
-import pandas as pd
-import json
-import csv
-import ast
 import time
 
 class RestaurantScraper:
